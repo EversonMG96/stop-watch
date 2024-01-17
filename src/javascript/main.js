0b66c0f221c79b2ab036cc2c337cf562e388b6bb
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-17 12:57:58 -0300

    add timer conditions and parameters

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,5 +2,22 @@ let [seconds, minutes, hours] = [0, 0, 0];
 let displayTime = document.getElementById("displayTime");
 
 function stopwatch() {
-    
+    second++;
+
+    if (seconds == 60) {
+        seconds = 0; 
+        
+        minutes ++;
+
+        if (minutes == 60) {
+            minutes = 0;
+        }
+
+        hours++;
+
+        if (hours == 24) {
+            hours = 0;
+        }
+         
+    }
 } 
